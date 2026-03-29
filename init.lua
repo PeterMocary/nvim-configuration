a0c68f556529ce54b4f7ae98fa2d7a31a9e27797
Peter Mocary <petermocary@gmail.com>
2026-03-29 20:53:07 +0200

    feat: enforce 4-space indentation for Python files
    Adds a FileType autocmd that sets expandtab and 4-space indent for all
    Python buffers. Project-level .editorconfig files will override this
    automatically via Neovim's built-in editorconfig support.

--- init.lua
+++ init.lua
@@ -225,6 +225,17 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
+-- Python: 4 spaces indentation (overridden by .editorconfig if present in project root)
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'python',
+  callback = function()
+    vim.bo.expandtab = true
+    vim.bo.shiftwidth = 4
+    vim.bo.tabstop = 4
+    vim.bo.softtabstop = 4
+  end,
+})
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
